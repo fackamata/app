1b0c93f880daba31e6535694e7863f07216469ab
Moha74-cyber <83348477+Moha74-cyber@users.noreply.github.com>
2021-09-20 15:06:06 +0200

    ajout

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -47,7 +47,7 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('annonces');
+            return $this->redirectToRoute('annonce_index');
         }
 
         return $this->render('registration/register.html.twig', [
