1d087c6f8c66675d6b327c7690445d73fdd72845
fackamata <marlene.lizzi@gmail.com>
2021-12-31 10:05:56 +0100

    service

--- src/Service/FileService.php
+++ src/Service/FileService.php
@@ -50,8 +50,9 @@ class FileService
     public function getSafeFileName(UploadedFile $file): string
     {
         $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
-        $safeFilename = transliterator_transliterate('Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()', $originalFilename);
-        $fileName = $safeFilename.'-'.uniqid().'.'.$file->guessExtension();
+        // $safeFilename = transliterator_transliterate('Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()', $originalFilename);
+        // $fileName = $safeFilename.'-'.uniqid().'.'.$file->guessExtension();
+        $fileName = $originalFilename.'-'.uniqid().'.'.$file->guessExtension();
 
         return $fileName;
     }
